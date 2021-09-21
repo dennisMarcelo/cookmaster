4824aa84ff474c1bee9d0d3c2dd69aaec53aba7f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-21 16:50:31 -0300

    adcionado conexão com o banco

---
+++ src/models/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+let schema = null;
+
+const getConnection = async () => {
+  if (schema) return Promise.resolve(schema);
+
+  return MongoClient
+    .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => console.log({ ErrorMessage: err.message }));
+};
+
+module.exports = {
+  getConnection,
+};
