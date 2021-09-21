75e7bc81e35704faeb2b00d41c0a28ce2be60cf0
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-21 19:03:40 -0300

    implementation of model to create new user and error middleware

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
+const userController = require('../controllers/userController');
+const errorMiddleware = require('../middlewares/error');
+
+app.use(bodyParser.json());
+
+// Não remover esse end-point, ele é necessário para o avaliador / R: não removido
 app.get('/', (request, response) => {
   response.send();
 });
-// Não remover esse end-point, ele é necessário para o avaliador
+// Não remover esse end-point, ele é necessário para o avaliador/ R: não removido
+
+app.route('/users')
+  .post(userController.create);
+
+app.use(errorMiddleware);
 
 module.exports = app;
