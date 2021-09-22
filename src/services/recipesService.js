bf5c476fc4b753a615f86fd3a28ad1a8a9f62070
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-22 15:26:26 -0300

    added service to get recipe by id

--- src/services/recipesService.js
+++ src/services/recipesService.js
@@ -2,6 +2,7 @@ const recipesModel = require('../models/recipesModel');
 
 const create = async (recipe) => {
   const newRecipe = await recipesModel.create(recipe);
+  
   return newRecipe;
 };
 
@@ -11,7 +12,14 @@ const getAll = async () => {
  return allRecipes;
 };
 
+const getById = async (id) => {
+  const recipe = await recipesModel.getById(id);
+
+  return recipe;
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
