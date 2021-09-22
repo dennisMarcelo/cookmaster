9e8b56d72c34755fd5a33f7944df16c50d287030
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-22 15:28:36 -0300

    added crontroller to get recipe by id

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,6 +1,6 @@
 const rescue = require('express-rescue');
 
-const { newRecipeValidation } = require('../helpers/validateFields');
+const { newRecipeValidation, isValiId } = require('../helpers/validateFields');
 const recipesService = require('../services/recipesService');
 
 const create = rescue(async (req, res) => {
@@ -20,7 +20,18 @@ const getAll = rescue(async (_req, res) => {
   res.status(200).json(allRecipes);
 });
 
+const getById = rescue(async (req, res) => {
+  isValiId(req.params);
+  const { id } = req.params;
+  const recipe = await recipesService.getById(id);
+
+  if (!recipe) return res.status(404).json({ message: 'recipe not found' });
+
+  res.status(200).json(recipe);
+});
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
