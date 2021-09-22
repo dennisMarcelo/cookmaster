569cdc8d92b3b0316bca25c2f7867edeb7c8bef8
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-22 18:19:00 -0300

    added middleware to validate user in routes  of recipes.

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const recipesModel = require('../models/recipesModel');
+
 const secretKey = 'forever wakanda';
 
 const validateJWT = async (req, res, next) => {
@@ -18,6 +20,29 @@ const validateJWT = async (req, res, next) => {
   }
 };
 
+const isAdminOrUser = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'missing auth token' });
+    
+  try {
+    const decoded = jwt.decode(token, secretKey);
+    const { email, role, _id: userId } = decoded;
+
+    const { id } = req.params;
+    const recipe = await recipesModel.getById(id);
+    
+    if (role === 'admin' || recipe.userId === userId) {
+      req.user = { email, role, userId };
+      return next();
+    }
+    
+    return res.status(401).json({ message: 'jwt malformed' });
+  } catch (err) {
+    return res.status(401).json({ message: 'jwt malformed' });
+  }
+};
+
 module.exports = {
   validateJWT,
+  isAdminOrUser,
 };
