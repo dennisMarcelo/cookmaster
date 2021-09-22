a512ae04ab93e7831b40c3349887f331e11058fd
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-21 23:10:08 -0300

    added  login service model and renamed variable existEmail in create user function

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,30 @@
 const userModel = require('../models/usersModel');
 const CustomError = require('../CustomError/CustomError');
+const { tokenGenerator } = require('../helpers/token');
 
 const create = async ({ name, email, password, role }) => {
-  const existEmail = await userModel.findByEmail(email);
-  console.log(existEmail);
-  if (existEmail) throw new CustomError('Email already registered', 409);
+  const user = await userModel.findUserByEmail(email);
+  if (user) throw new CustomError('Email already registered', 409);
 
   const result = await userModel.create({ name, email, password, role });
   return result;
 };
 
+const login = async ({ email, password }) => {
+  const userOfDatabase = await userModel.findUserByEmail(email);
+  
+  if (!userOfDatabase) throw new CustomError('Incorrect username or password', 401);
+
+  if (userOfDatabase.email !== email || userOfDatabase.password !== password) {
+    throw new CustomError('Incorrect username or password', 401);
+  }
+
+  const token = tokenGenerator(userOfDatabase);
+
+  return { token };
+};
+
 module.exports = {
   create,
+  login,
 };
