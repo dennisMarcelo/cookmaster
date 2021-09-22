225af0c64fb0dc31aca675320c56c66dad236f0b
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-21 23:13:04 -0300

    added login controller function and login validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,8 @@ const rescue = require('express-rescue');
 const userService = require('../services/userService');
 const CustomError = require('../CustomError/CustomError');
 
-const isValidFields = (request) => {
+// validations
+const newUserValidation = (request) => {
   const { error } = Joi.object({
     name: Joi.string().not().empty().required(),
     password: Joi.string().not().empty().required(),
@@ -18,14 +19,39 @@ const isValidFields = (request) => {
   if (error) throw new CustomError('Invalid entries. Try again.', 400);
 };
 
+const loginValidation = (request) => {
+  const { error } = Joi.object({
+    password: Joi.string().not().empty().required(),
+    email: Joi
+      .string()
+      .email()
+      .not()
+      .empty()
+      .required(),
+  }).validate(request);
+
+  if (error) throw new CustomError('All fields must be filled', 401);
+};
+
+// middlewares
 const create = rescue(async (req, res) => {
-  isValidFields(req.body);
+  newUserValidation(req.body);
 
   const { name, email, password } = req.body;
   const result = await userService.create({ name, email, password, role: 'user' });
   res.status(201).json(result);
 });
 
+const login = rescue(async (req, res) => {
+  loginValidation(req.body);
+
+  const { email, password } = req.body;
+  const token = await userService.login({ email, password });
+
+  res.status(200).json(token);
+});
+
 module.exports = {
   create,
+  login,
 };
