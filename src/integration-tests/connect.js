42b4f2d01e94efdce79933cd2770e1e4986a7dfc
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-23 20:24:08 -0300

    integration tests for user routes

---
+++ src/integration-tests/connect.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const DBServer = new MongoMemoryServer();
+const OPTIONS = {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+};
+
+const getConnection = async () => {
+const URLmock = await DBServer.getUri();
+return MongoClient.connect(URLmock, OPTIONS)
+}
+
+module.exports = getConnection; 
--- src/integration-tests/users.test.js
+++ src/integration-tests/users.test.js
@@ -3,39 +3,32 @@ const { expect } = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const { MongoClient } = require('mongodb');
-const { MongoMemoryServer} = require('mongodb-memory-server');
+
+const connectionMock = require('./connect')
 
 const server  = require('../api/app');
 
 chai.use(chaiHttp);
 
-const user1= {name:"t'challa", email: 'black@panther.marvel.com', password:'wakanda forever'}
+const DB_NAME = 'Cookmaster';
+const idFake = '614bf57bb41a7734551f85c1';
+const user1 = {name:"t'challa", email: 'black@panther.marvel.com', password:'wakanda forever'};
 
 describe('User routes', () => {
-  const DBServer = new MongoMemoryServer();
-
-  before(async () => {
-    console.log('1');
-    const URLMock = await DBServer.getUri();
-    const connectionMock = await MongoClient.connect(URLMock, {
-      useNewUrlParser: true, useUnifiedTopology: true,
-    });
-
-    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
-  })
-
-  after(async () => {
-    console.log('2');
-    MongoClient.connect.restore();
-    await DBServer.stop();
-  })
-
   describe('user created with success return', () => {
     let response = {};
+    
     before(async () => {
+      const connection = await connectionMock()
+      sinon.stub(MongoClient, 'connect').resolves(connection);
+
       response = await chai.request(server)
         .post('/users')
         .send(user1)
+    })
+  
+    after(async () => {
+      MongoClient.connect.restore();
     })
 
     it('status code 201', () => {
@@ -53,4 +46,42 @@ describe('User routes', () => {
       expect(response.body.user).to.have.property('_id');
     });
   });
+
+  describe('login with success return', () => {
+    let response = {};
+    
+    before(async () => {
+      const connection = await connectionMock();
+
+      sinon.stub(MongoClient, 'connect').resolves(connection);
+
+      await connection.db(DB_NAME)
+        .collection('users')
+        .insertOne({
+          _id: idFake,
+          name: user1.name,
+          email: user1.email,
+          password: user1.password,
+          role: 'user'
+        });
+      
+        response = await chai.request(server)
+          .post('/login')
+          .send({ email: user1.email, password: user1.password });
+    });
+  
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+  
+    it('status code 200', async () => {
+
+      expect(response).to.have.status(200);
+    });
+
+    it('token as an answer', () => {
+      expect(response.body).to.have.property('token');
+    });
+  });
+
 });
