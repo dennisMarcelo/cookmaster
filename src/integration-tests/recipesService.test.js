96aa0a3a79c4e44d9a520a2ea15c52eef3ace98a
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-24 11:36:47 -0300

    teste to get all recipes

--- src/integration-tests/recipesService.test.js
+++ src/integration-tests/recipesService.test.js
@@ -9,12 +9,19 @@ const {getConnection, DBServer} = require('./connect')
 const server  = require('../api/app');
 
 const DB_NAME = 'Cookmaster';
+const recipeId1 = '6432ef45abaa7234551f25c1';
+const recipeId2 = '1232ffeb23ba57234551faf4';
 const userId = '614bf57bb41a7734551f85c1';
 const user1 = {name:"t'challa", email: 'black@panther.marvel.com', password:'wakanda forever'};
 const recipe1 = {
   name: 'grilled fish',
   ingredients: 'fish oil',
   preparation: 'fry the fish until golden'
+}
+const recipe2 = {
+  name: 'boiled fish',
+  ingredients: 'fish, water, tomato',
+  preparation: 'boil fish with all ingredients'
 }
 
 describe('Recipes routes', ()=>{
@@ -66,4 +73,51 @@ describe('Recipes routes', ()=>{
       expect(response.body.recipe).to.have.property('_id');
     })
   })
+
+  describe('route for get all recipes returns', () =>{
+    let response = {};
+    let connection = null
+    
+    before(async () => {
+      connection = await getConnection();
+      sinon.stub(MongoClient, 'connect').resolves(connection);
+
+      await connection.db(DB_NAME)
+      .collection('recipes')
+      .insertMany([
+        {
+          _id: recipeId1,
+          name: recipe1.name,
+          ingredients: recipe1.ingredients,
+          preparation: recipe1.preparation,
+          userId: userId,
+        },
+        {
+          _id: recipeId2,
+          name: recipe2.name,
+          ingredients: recipe2.ingredients,
+          preparation: recipe2.preparation,
+          userId: userId,
+        }
+      ]);
+
+      response = await chai.request(server)
+        .get('/recipes')
+
+    });
+
+    after(async () => {
+      await connection.db(DB_NAME).collection('recipes').drop();
+      MongoClient.connect.restore();
+    });
+
+    it('status 200', () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('one array with all recipes', () => {
+      expect(response.body).to.be.an('array');
+      expect(response.body).to.be.length(2);
+    });
+  });
 })
