429cc7c59164b42d4f316e64c34c3b8754e402e9
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-26 14:56:28 -0300

    added middleware for validate admin token

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -3,11 +3,12 @@ const jwt = require('jsonwebtoken');
 const recipesModel = require('../models/recipesModel');
 
 const secretKey = 'forever wakanda';
+const message = 'jwt malformed';
 
 const validateJWT = async (req, res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ message: 'jwt malformed' });
+  if (!token) return res.status(401).json({ message });
 
   try {
     const decoded = jwt.decode(token, secretKey);
@@ -16,7 +17,7 @@ const validateJWT = async (req, res, next) => {
     req.user = { email, role, userId };
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message });
   }
 };
 
@@ -37,13 +38,24 @@ const isAdminOrUser = async (req, res, next) => {
       return next();
     }
     
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message });
   } catch (err) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message });
   }
+};
+
+const isAdmin = async (req, res, next) => {
+  const { role } = req.user;
+  
+  if (role !== 'admin') {
+    return res.status(403).json({ message: 'Only admins can register new admins' });
+  }
+  
+  next();
 };
 
 module.exports = {
   validateJWT,
   isAdminOrUser,
+  isAdmin,
 };
