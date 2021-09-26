970d11cd06c93399d889dba654b5940379013f49
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-26 15:17:31 -0300

    added test for "normal user can't add new admin user"

--- src/integration-tests/admin.test.js
+++ src/integration-tests/admin.test.js
@@ -15,7 +15,7 @@ const idFake = '614bf57bb41a7734551f85c1';
 const user1 = {name:"t'challa", email: 'black@panther.marvel.com', password:'wakanda forever'};
 
 describe('Admin route', () => {
-  describe('user admin add new user as root and return', () => {
+  describe('user admin add new user as admin and return', () => {
     let response = {};
     let connection = null
     let token = null;
@@ -59,6 +59,51 @@ describe('Admin route', () => {
       expect(response.body.user.email).to.be.equal(user1.email);
       expect(response.body.user.role).to.be.equal("admin");
       expect(response.body.user).to.have.property('_id');
+    })
+  });
+
+  describe('normal user can\'t add new user and return', () => {
+    let response = {};
+    let connection = null
+    let token = null;
+
+    before(async () => {
+      connection = await getConnection();
+      sinon.stub(MongoClient, 'connect').resolves(connection);
+
+      await connection.db(DB_NAME)
+        .collection('users')
+        .insertOne({
+          _id: idFake,
+          name: 'admin',
+          email: 'admin@admin.com',
+          password: 'adminSuperAdmin',
+          role: 'user'
+        });
+
+      token = await chai.request(server)
+        .post('/login')
+        .send({email:'admin@admin.com', password: 'adminSuperAdmin'})
+        .then((res) => res.body.token);
+      
+      response = await chai.request(server)
+        .post('/users/admin')
+        .set('authorization', token)
+        .send(user1);
+    })
+
+    after(async () => {
+      await connection.db(DB_NAME).collection('users').drop();
+      MongoClient.connect.restore();
+    })
+
+    it('status 403', () => {
+      expect(response).to.be.status(403);
+    })
+
+    it('object with details about new admin user', () => {
+      expect(response.body.message).to.be.equal('Only admins can register new admins');
+     
     })
   });
 })
